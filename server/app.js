0468ed1b4aae879888515ab2c15d86122a141f1d
sorenwojo <soren@wolder-jorgensen.dk>
2016-05-07 20:52:33 +0200

    Moved turnout creation and handling out of app

--- server/app.js
+++ server/app.js
@@ -1,19 +1,13 @@
-var Turnout = require("./turnout");
-var Settings = require("./settings");
+var settings = require("./settings");
 var keypress = require('keypress');
 var turnoutHandler = require('./turnoutHandler');
 var server = require('./server');
 keypress(process.stdin);
 
+turnoutHandler.initialize(settings);
 server.createServer(turnoutHandler);
 
-var settingsForTurnouts = Settings.getSettingsForAllTurnouts();
-var turnouts = settingsForTurnouts.map(function(item){
-  return new Turnout(item.name);
-});
 
-setInterval(timerTrig, 500);
-console.log("Count:", turnouts.length);
 process.stdin.on('keypress', function (ch, key) {
   if (key && key.name == 's') {
     turnouts[0].toggleSwitch();
@@ -27,9 +21,3 @@ process.stdin.on('keypress', function (ch, key) {
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
-
-function timerTrig() {
-    turnouts.forEach(function(element) {
-        element.intervalPassed();
-    }, this);    
-}
--- server/turnoutHandler.js
+++ server/turnoutHandler.js
@@ -1,3 +1,23 @@
+var Turnout = require("./turnout");
+var turnouts;
+
+module.exports.initialize = function(settings) {
+    var settingsForTurnouts = settings.getSettingsForAllTurnouts();
+    turnouts = settingsForTurnouts.map(function(item){
+        return new Turnout(item.name);
+    });    
+    
+    console.log("Count:", turnouts.length);
+
+    setInterval(timerTrig, 500);
+};
+
+function timerTrig() {
+    turnouts.forEach(function(element) {
+        element.intervalPassed();
+    }, this);    
+}
+
 module.exports.getStatus = function() {
     return "OY!";
-}
+};
