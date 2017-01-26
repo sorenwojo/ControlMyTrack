a6503b4ffcef487f775c45c365992c1a48b86c72
sorenwojo <soren@wolder-jorgensen.dk>
2017-01-26 21:46:30 +0100

    Publish all files

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -9,15 +9,24 @@ var plugins = require("gulp-load-plugins")({
 var dest = 'public/';
 
 gulp.task('publish-js', function() {
-	var jsFiles = ['src/js/*'];
+	var jsFiles = ['src/script/*'];
 
     gulp.src(plugins.mainBowerFiles().concat(jsFiles))
-		.pipe(plugins.filter('**/*.js'))
 		.pipe(gulp.dest(dest + 'script'));
+});
+
+gulp.task('publish-html', function() {
+	gulp.src(["src/*.html", "src/.htm"])
+		.pipe(gulp.dest(dest));
+});
+
+gulp.task('publish-css', function() {
+	gulp.src(["src/style/*"])
+		.pipe(gulp.dest(dest + '/style'));
 });
 
 gulp.task('clean-output', function(){
     del.sync(dest);
 });
 
-gulp.task('default', ['publish-js']);
+gulp.task('default', ['publish-js', 'publish-html', 'publish-css']);
